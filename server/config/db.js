4d479c1984bf58147dd0e07ce5e7ec676f838f7b
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-08-09 17:36:41 +0530

    chore: initialize AURA GO backend

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("Mongodb connected successfully");
+
+    } catch (error) {
+        console.log("MongoDB connection failed:", error.message);
+        process.exit(1)
+    }
+};
+
+
+module.exports = connectDB;
---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorMiddleware");
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(notFound);
+app.use(errorHandler);
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "AURA GO API is running"
+    });
+});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`AURA GO server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("Server startup failed:", error.message);
+        process.exit(1);
+    }
+};
+
+startServer();
