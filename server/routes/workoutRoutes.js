65af00004526e4ab008664ba780172b8a2804cba
ADARSH DAROKAR <adarshdarokar55@gmail.com>
2026-09-16 20:16:15 +0530

    feat: add exercises to workouts

--- server/routes/workoutRoutes.js
+++ server/routes/workoutRoutes.js
@@ -1,45 +1,34 @@
 const express = require("express");
 
 const {
-    createWorkoutController,
-    getUserWorkoutsController,
-    getWorkoutByIdController,
-    updateWorkoutController,
-    deleteWorkoutController
+  createWorkoutController,
+  getUserWorkoutsController,
+  getWorkoutByIdController,
+  updateWorkoutController,
+  deleteWorkoutController,
+  addExerciseToWorkoutController,
+  removeExerciseFromWorkoutController,
 } = require("../controllers/workoutController");
 
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post(
-    "/",
-    protect,
-    createWorkoutController
-);
+router.post("/", protect, createWorkoutController);
 
-router.get(
-    "/",
-    protect,
-    getUserWorkoutsController
-);
+router.get("/", protect, getUserWorkoutsController);
 
-router.get(
-    "/:id",
-    protect,
-    getWorkoutByIdController
-);
+router.get("/:id", protect, getWorkoutByIdController);
 
-router.patch(
-    "/:id",
-    protect,
-    updateWorkoutController
-);
+router.patch("/:id", protect, updateWorkoutController);
+
+router.delete("/:id", protect, deleteWorkoutController);
+router.post("/:id/exercises", protect, addExerciseToWorkoutController);
 
 router.delete(
-    "/:id",
-    protect,
-    deleteWorkoutController
+  "/:id/exercises/:exerciseId",
+  protect,
+  removeExerciseFromWorkoutController,
 );
 
 module.exports = router;
--- server/services/workoutService.js
+++ server/services/workoutService.js
@@ -84,10 +84,71 @@ const deleteWorkout = async (userId, workoutId) => {
     return workout;
 };
 
+const addExerciseToWorkout = async (
+    userId,
+    workoutId,
+    exerciseData
+) => {
+    const workout = await Workout.findOne({
+        _id: workoutId,
+        user: userId
+    });
+
+    if (!workout) {
+        const error = new Error("Workout not found");
+        error.statusCode = 404;
+        throw error;
+    }
+
+    workout.exercises.push(exerciseData);
+
+    await workout.save();
+
+    return workout;
+};
+
+const removeExerciseFromWorkout = async (
+    userId,
+    workoutId,
+    exerciseId
+) => {
+    const workout = await Workout.findOne({
+        _id: workoutId,
+        user: userId
+    });
+
+    if (!workout) {
+        const error = new Error("Workout not found");
+        error.statusCode = 404;
+        throw error;
+    }
+
+    const exerciseIndex = workout.exercises.findIndex(
+        (item) =>
+            item.exercise.toString() === exerciseId
+    );
+
+    if (exerciseIndex === -1) {
+        const error = new Error(
+            "Exercise not found in workout"
+        );
+        error.statusCode = 404;
+        throw error;
+    }
+
+    workout.exercises.splice(exerciseIndex, 1);
+
+    await workout.save();
+
+    return workout;
+};
+
 module.exports = {
     createWorkout,
     getUserWorkouts,
     getWorkoutById,
     updateWorkout,
-    deleteWorkout
+    deleteWorkout,
+    addExerciseToWorkout,
+    removeExerciseFromWorkout
 };
